99661f6fb2e9f812772e236b56df9355030c8b43
Aleksandr Fominov <insomniansk89@gmail.com>
2021-11-02 00:18:03 +0700

    fixed install mongo script

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Additional repos
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \ 
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \
 | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 #Update system packages
 apt update && apt upgrade
