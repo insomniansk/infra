026b1ea512952602bb71302ecaf1ed8d65817b3a
Aleksandr Fominov <insomniansk89@gmail.com>
2021-11-02 00:24:14 +0700

    Fixed scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Variables
-project_path = "/home/reddit-app"
+project_path="/home/reddit-app"
 #Deploying project
 mkdir $project_path && cd $project_path
 git clone -b monolith https://github.com/express42/reddit.git && cd reddit
