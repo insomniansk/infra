3be81d6d7e2bd63e1e8b9158b88bd5c5cc2bb804
Aleksandr Fominov <insomniansk89@gmail.com>
2021-11-01 17:36:18 +0700

    fixed startup_script

--- startup_script.sh
+++ startup_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #Variables
-project_path = "/home/reddit-app"
+project_path="/home/reddit-app"
 #Additional repos
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \ 
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \
 | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 #Update system packages
-apt update && apt upgrade
+apt update && apt upgrade -y
 #Install Ruby packages
 apt install -y ruby-full build-essential
-gem install bundler:1.16.1
+gem install -y bundler:1.16.1
 # Install and configure MongoDb
 apt install -y mongodb-org
 systemctl start mongod
