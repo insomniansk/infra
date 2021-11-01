b7cf4143708f447487cf38499cad20f370d74c20
Aleksandr Fominov <insomniansk89@gmail.com>
2021-11-02 00:01:56 +0700

    fixed ruby_install.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -2,5 +2,5 @@
 #Update system packages
 apt update && apt upgrade
 #Install Ruby packages
-apt install -y ruby-full build-essential
+apt install -y ruby-full ruby-bundler build-essential
 gem install bundler:1.16.1
