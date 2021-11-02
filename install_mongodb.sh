763a6f5c20041c102d35fc944d5bb131df625406
Aleksandr Fominov <insomniansk89@gmail.com>
2021-11-02 13:26:04 +0700

    Added branches in install_mongodb.sh and change dist in .travis.yml to pass tests

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #Additional repos
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+if  lsb_release -c | grep -q 'focal'
+then
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \
+| sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+elif lsb_release -c | grep -q 'bionic'
+then
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" \
+| sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+elif lsb_release -c | grep -q 'xenial'
+then
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" \
 | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 #Update system packages
 apt install -y apt-transport-https ca-certificates
